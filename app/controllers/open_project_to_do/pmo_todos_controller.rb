0cc339d2c4007e374f05fc5ac9f354f2c0007630
Nishant Goel <goelnishan26@gmail.com>
2016-10-24 18:23:11 +0530

    inline editing of todos

--- app/controllers/open_project_to_do/pmo_todos_controller.rb
+++ app/controllers/open_project_to_do/pmo_todos_controller.rb
@@ -2,32 +2,48 @@ require_dependency "open_project_to_do/application_controller"
 
 module OpenProjectToDo
   class PmoTodosController < ApplicationController
-  	before_action :set_todo, except: :create
+    before_action :set_todo, except: :create
     def create
       @pmo_todo = PmoTodo.new(todo_params)
-      @pmo_todo.save
-
-      respond_to do |format|
-      	format.js
+      if @pmo_todo.save
+        redirect_to request.referer, notice: 'Todo created successfully'
       end
     end
 
     def destroy
-    	@todo_id = @todo.id
-    	@todo.delete
-    	
-    	respond_to do |format|
-      	format.js
+      @todo_id = @todo.id
+      @todo.delete
+
+      redirect_to request.referer, notice: 'Todo deleted successfully'
+    end
+
+    def update
+      if @todo.update_attributes(todo_params)
+        redirect_to request.referer, notice: 'Todo updated successfully'
       end
     end
-  	
-  	private
-  	def set_todo
-  		@todo = PmoTodo.find_by(id: params[:id])
-  	end
+    
+    def update_order
+      PmoTodo.find_by(id: params[:pmo_todo][:todo_id]).update_attribute(:line_id, params[:pmo_todo][:line_id])
+      
+      todos = PmoTodo.where(work_package_id: params[:work_pack]).order('line_id asc')
+      
+      next_todo_line_id = 1
+      todos.each do |t| 
+        t.update_attribute(:line_id, next_todo_line_id)
+        next_todo_line_id += 1
+      end
 
-  	def todo_params
-  		params.require(:todos).permit(:work_package_id, :sort, :line_id, :work, :work_unit, :duration, :duration_unit, :assignee_id, :text, :finished)
-  	end
+      redirect_to request.referer, notice: 'Todos order updated successfully'
+    end
+
+    private
+    def set_todo
+      @todo = PmoTodo.find_by(id: params[:id])
+    end
+
+    def todo_params
+      params.require(:pmo_todo || :todos).permit(:work_package_id, :sort, :line_id, :work, :work_unit, :duration, :duration_unit, :assignee_id, :text, :finished)
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 OpenProjectToDo::Engine.routes.draw do
 
-	post '/create' => 'pmo_todos#create', as: :create_todos
-	delete '/todos/:id' => 'pmo_todos#destroy', as: :destroy_todo, format: :js
-
+	post '/create' => 'pmo_todos#create', as: :create_todo
+	patch '/:id/update' => 'pmo_todos#update', as: :update_todo
+	delete '/todos/:id' => 'pmo_todos#destroy', as: :destroy_todo
+	patch '/update_todos_order' => 'pmo_todos#update_order', as: :update_todos_order
 end
